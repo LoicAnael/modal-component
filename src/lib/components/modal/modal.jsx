9d88473ec72e417aa401ebf59b23aaf50c738464
loic <loicanaelbdf@gmail.com>
2022-12-20 11:25:40 -0800

    modal component done

---
+++ src/lib/components/modal/modal.jsx
@@ -0,0 +1,17 @@
+import './modal.css'
+import React from 'react'
+
+const Modal = ({text, closeModal}) => (
+    <div className='modal__container'>
+        <div className='modal'>
+            <div className='modal__header'>
+                <button className='modal__header__button' onClick={closeModal}>X</button>
+            </div>
+            <div className='modal__text'>
+                <h2>{text}</h2>
+            </div>
+        </div>
+    </div>
+)
+
+export default Modal
