0e31f38fc9b63ae2f7e66583d508edefdf390ced
loic <loicanaelbdf@gmail.com>
2022-12-20 12:01:24 -0800

    style completed

--- src/lib/components/modal/modal.jsx
+++ src/lib/components/modal/modal.jsx
@@ -2,16 +2,15 @@ import './modal.css'
 import React from 'react'
 
 const Modal = ({text, closeModal}) => (
-    <div className='modal__container'>
+
         <div className='modal'>
-            <div className='modal__header'>
-                <button className='modal__header__button' onClick={closeModal}>X</button>
+            <div className='modal-header'>
+                <button className='modal-header__button' onClick={closeModal}>X</button>
             </div>
-            <div className='modal__text'>
+            <div className='modal-text'>
                 <h2>{text}</h2>
             </div>
         </div>
-    </div>
 )
 
 export default Modal
