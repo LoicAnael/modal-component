2c76e8058921476024dd19601326e47d02ca750c
loic <loicanaelbdf@gmail.com>
2022-12-20 13:17:03 -0800

    package.json settings

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import { useState } from 'react';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { useState } from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import Modal from './lib/components/modal/modal'
 
 function App() {
-    
-    const [isShow, setIsShow] = useState(true);
-    const closeModal = () => {
-        setIsShow(!isShow)
-    }
-    return isShow ? ReactDOM.createPortal(
+  const [isShow, setIsShow] = useState(true)
+  const closeModal = () => {
+    setIsShow(!isShow)
+  }
+  return isShow
+    ? ReactDOM.createPortal(
         <>
-            <Modal text='Your text here!' closeModal={(e) => closeModal()} />
-        </>, document.body
-        
-    )
-    :
-    null
+          <Modal text="Your text here!" closeModal={(e) => closeModal()} />
+        </>,
+        document.body
+      )
+    : null
 }
 
-export default App;
+export default App
--- src/lib/components/modal/modal.jsx
+++ src/lib/components/modal/modal.jsx
@@ -1,16 +1,17 @@
 import './modal.css'
 import React from 'react'
 
-const Modal = ({text, closeModal}) => (
-
-        <div className='modal'>
-            <div className='modal-header'>
-                <button className='modal-header__button' onClick={closeModal}>X</button>
-            </div>
-            <div className='modal-text'>
-                <h2>{text}</h2>
-            </div>
-        </div>
+const Modal = ({ text, closeModal }) => (
+  <div className="modal">
+    <div className="modal-header">
+      <button className="modal-header__button" onClick={closeModal}>
+        X
+      </button>
+    </div>
+    <div className="modal-text">
+      <h2>{text}</h2>
+    </div>
+  </div>
 )
 
 export default Modal
