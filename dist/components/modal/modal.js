c4a7cee7c389975176d9f307181fea2116bebe4e
loic <loicanaelbdf@gmail.com>
2022-12-20 16:57:36 -0800

    publish config

---
+++ dist/components/modal/modal.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault").default;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+require("./modal.css");
+var _react = _interopRequireDefault(require("react"));
+var Modal = function Modal(_ref) {
+  var text = _ref.text,
+    closeModal = _ref.closeModal;
+  return /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal"
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal-header"
+  }, /*#__PURE__*/_react.default.createElement("button", {
+    className: "modal-header__button",
+    onClick: closeModal
+  }, "X")), /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal-text"
+  }, /*#__PURE__*/_react.default.createElement("h2", null, text)));
+};
+var _default = Modal;
+exports.default = _default;
